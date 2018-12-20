953248a625e129d7753e18996ea51cde31344557
oarodriguez <omargumer22@gmail.com>
2018-12-19 23:52:36 -0430

    Se agrego variable protegida para el Sluggable.

--- app/Article.php
+++ app/Article.php
@@ -3,9 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
 
-class Article extends Model
+class Article extends Model implements SluggableInterface
 {
+
+    use SluggableTrait;
+ 	// Variable creada para el Sluggable.
+    protected $sluggable = [
+        'build_from' => 'title', // Esto es de que campo de la BD se sacara el Slugg.
+        'save_to'    => 'slug', // Esto es el Campo donde se guardara ese slugg en la BD.
+    ];
+
     // Tabla & Campos.
 	protected $table = "articles";
 	protected $fillable = ['title', 'content', 'category_id', 'user_id'];
