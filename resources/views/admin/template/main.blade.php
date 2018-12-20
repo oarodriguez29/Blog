1b4caff1f5e3f12b5bd272310344fb9bb2c18753
oarodriguez <omargumer22@gmail.com>
2018-12-19 23:58:51 -0430

    Modificacion en las Vistas para Estructura de Plantilla.

--- resources/views/admin/template/main.blade.php
+++ resources/views/admin/template/main.blade.php
@@ -4,6 +4,54 @@
 	<meta charset="utf-8">
 	<title>@yield('title', 'Default') | Panel de Administración</title>
 	<link rel="stylesheet" type="text/css" href="{{ asset('plugins/bootstrap/css/bootstrap.css') }}">
+
+<style type="text/css">
+	/* Sticky footer styles
+	-------------------------------------------------- */
+	html {
+	  position: relative;
+	  min-height: 100%;
+	}
+	body {
+	  /* Margin bottom by footer height */
+	  margin-bottom: 60px;
+	  background-image: url('img/fondohbm.jpg');
+	  opacity: 0.9;
+	}
+	.jumbotron {
+		margin-top: 60px;		
+	}
+	.footer {
+	  position: absolute;
+	  bottom: 0;
+	  width: 100%;
+	  /* Set the fixed height of the footer here */
+	  height: 60px;
+	  background-color: #f5f5f5;
+	}
+
+
+	/* Custom page CSS
+	-------------------------------------------------- */
+	/* Not required for template or sticky footer method. */
+
+	body > .container {
+	  padding: 60px 15px 0;
+	}
+	.container .text-muted {
+	  margin: 20px 0;
+	}
+
+	.footer > .container {
+	  padding-right: 15px;
+	  padding-left: 15px;
+	}
+
+	code {
+	  font-size: 80%;
+	}	
+</style>
+
 </head>
 <body>
 	@include('admin.template.partials.nav')
@@ -15,4 +63,23 @@
 	<script src="{{ asset('plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
 	<script src="{{ asset('plugins/bootstrap/js/bootstrap.min.js') }}"></script>
 </body>
+
+<footer class="footer">
+	<div class="container">	  
+	  	<div class="row justify-content-center">
+	  		<div class="panel-footer">
+		  		<div class="col-sm-6">
+		    		<h6>Todos los Derechos Reservados.</h6>	  			
+		  		</div>
+		  		<div class="col-sm-5">
+		  			<h6>Laravel v5.1</h6>
+		  		</div>
+		  		<div class="text-right">
+		  			<h6>Copyright © 2018</h6>
+		  		</div>	  			
+	  		</div>
+	  	</div>	  
+	</div>
+</footer>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,50 @@
 @endsection
 
 @section('content')
-    <h1>Inicio del Proyecto</h1>
-    <a href="" class="btn btn-success">Boton</a>    
+    <div class="container">
+        
+    
+        <div class="jumbotron">
+            <div class="container">
+                <h3 class="text-center">
+                Usando Clase Jumbotron! de BootsTrap3 para el Proyecto.
+                </h3>
+            </div>
+            <hr>
+            <div class="container">
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">First</th>
+                          <th scope="col">Last</th>
+                          <th scope="col">Handle</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row">1</th>
+                          <td>Mark</td>
+                          <td>Otto</td>
+                          <td>@mdo</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">2</th>
+                          <td>Jacob</td>
+                          <td>Thornton</td>
+                          <td>@fat</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">3</th>
+                          <td colspan="2">Larry the Bird</td>
+                          <td>@twitter</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                </div>
+                <a href="#" class="btn btn-success">Boton</a>    
+            </div>        
+        </div>
+    </div>
 @endsection
