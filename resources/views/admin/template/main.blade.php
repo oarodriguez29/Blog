237d362b055d1c751a5134e24bf88a66c53b25f7
oarodriguez <omargumer22@gmail.com>
2018-12-25 20:36:49 -0430

    Creacion de la Plantilla principal del Sistema.

--- resources/views/admin/template/main.blade.php
+++ resources/views/admin/template/main.blade.php
@@ -3,65 +3,30 @@
 <head>
 	<meta charset="utf-8">
 	<title>@yield('title', 'Default') | Panel de Administración</title>
+	{{-- BootsTrap.css v3.3.7 --}}
 	<link rel="stylesheet" type="text/css" href="{{ asset('plugins/bootstrap/css/bootstrap.css') }}">
-
-<style type="text/css">
-	/* Sticky footer styles
-	-------------------------------------------------- */
-	html {
-	  position: relative;
-	  min-height: 100%;
-	}
-	body {
-	  /* Margin bottom by footer height */
-	  margin-bottom: 60px;
-	  background-image: url('img/fondohbm.jpg');
-	  opacity: 0.9;
-	}
-	.jumbotron {
-		margin-top: 60px;		
-	}
-	.footer {
-	  position: absolute;
-	  bottom: 0;
-	  width: 100%;
-	  /* Set the fixed height of the footer here */
-	  height: 60px;
-	  background-color: #f5f5f5;
-	}
-
-
-	/* Custom page CSS
-	-------------------------------------------------- */
-	/* Not required for template or sticky footer method. */
-
-	body > .container {
-	  padding: 60px 15px 0;
-	}
-	.container .text-muted {
-	  margin: 20px 0;
-	}
-
-	.footer > .container {
-	  padding-right: 15px;
-	  padding-left: 15px;
-	}
-
-	code {
-	  font-size: 80%;
-	}	
-</style>
-
+	{{-- Estilos CSS Personalizados --}}
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/estilos.css') }}">
 </head>
 <body>
 	@include('admin.template.partials.nav')
 
-	<section>
+	<section> {{-- Seccion de mensajes --}}
+		@include('flash::message')
+		@yield('message')
+	</section>
+	<section> {{-- Seccion de Contenido --}}
 		@yield('content')
 	</section>
 
+	{{-- JQuery v2.2.3 --}}
 	<script src="{{ asset('plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
+	{{-- BootTrap.js v3.3.7 --}}
 	<script src="{{ asset('plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+	<script>
+		{{-- Oculta los mensajes o Alert "No Importantes" Mostrados al Usuario. --}}  
+		$('div.alert').not('.alert-important').delay(3000).fadeOut(350);
+	</script>
 </body>
 
 <footer class="footer">
@@ -75,7 +40,7 @@
 		  			<h6>Laravel v5.1</h6>
 		  		</div>
 		  		<div class="text-right">
-		  			<h6>Copyright © 2018</h6>
+		  			<h6>© 2018 Copyright</h6>
 		  		</div>	  			
 	  		</div>
 	  	</div>	  
