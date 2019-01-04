05329f2e517c9aa484aa71e5ace2162c42d591ae
oarodriguez <omargumer22@gmail.com>
2019-01-03 21:34:56 -0430

    algunas modificaciones...

--- resources/views/admin/template/partials/nav.blade.php
+++ resources/views/admin/template/partials/nav.blade.php
@@ -8,35 +8,32 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">
-            <img alt="foto" width="25" height="25" class="card-img img-circle" src="{{ asset('img/perfil.jpg') }}">
+
+          <a class="navbar-brand" href="{{ route('admin.auth.login') }}" title="Iniciar Sesión!">
+            <img alt="{{ (Auth::user()) ? Auth::user()->name : 'foto' }}" width="25" height="25" class="card-img img-circle" src="{{ asset('img/perfil.jpg') }}">
           </a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="/">Inicio</a></li>
-            <li><a href="{{ route('admin.users.index') }}">Usuarios</a></li>
-            <li><a href="{{ route('admin.categories.index') }}">Categorias</a></li>
-            <li><a href="#article">Articulos</a></li>
-            <li><a href="#image">Imagenes</a></li>
-            <li><a href="#tag">Tags</a></li>
+          @if (Auth::user())
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="/">Inicio</a></li>
+              <li><a href="{{ route('admin.users.index') }}">Usuarios</a></li>
+              <li><a href="{{ route('admin.categories.index') }}">Categorias</a></li>
+              <li><a href="#article">Articulos</a></li>
+              <li><a href="#image">Imagenes</a></li>
+              <li><a href="{{ route('admin.tags.index') }}">Tags</a></li>
 
-          </ul>
+            </ul>
 
-          <ul class="nav navbar-nav navbar-right">
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Opciones <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="{{ route('admin.auth.logout') }}">Salir</a></li>
+                </ul>
+              </li>
+            </ul>
+          @endif
         </div><!--/.nav-collapse -->
       </div>
     </nav>
