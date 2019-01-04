0e979dee44459901a5d03ec0428180cbd0f03da4
oarodriguez <omargumer22@gmail.com>
2019-01-03 21:34:36 -0430

    algunas modificaciones...

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -23,7 +23,7 @@
 
 					<div class="form-group">
 						{!! Form::label('type', 'Tipo') !!}
-						{!! Form::select('type', ['member'=>'Miembro', 'admin'=>'Administrador'], null, ['class'=>'form-control','required']) !!}
+						{!! Form::select('type', ['member'=>'Miembro', 'admin'=>'Administrador'], $user->type, ['class'=>'form-control','required']) !!}
 					</div>
 
 					<div class="form-group text-right">
