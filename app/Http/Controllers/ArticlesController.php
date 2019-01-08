8f12b7c3aad013da64e1ec5c540910e095520786
oarodriguez <omargumer22@gmail.com>
2019-01-07 21:31:15 -0430

    Continuacion para la logica del CRUD de los articulos..

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -8,6 +8,10 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Category;
 use App\Tag;
+use App\Article;
+use App\Image;
+use Laracasts\Flash\Flash;
+use App\Http\Requests\ArticleRequest;
 
 class ArticlesController extends Controller
 {
@@ -18,7 +22,8 @@ class ArticlesController extends Controller
      */
     public function index()
     {
-        //
+        // Retorno la Vista 'index'
+        return view('admin.articles.index');
     }
 
     /**
@@ -44,16 +49,47 @@ class ArticlesController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ArticleRequest $request)
     {
-        // Manipulacion de Imagen.
-        $file = $request->file('image');
-        // Nombre del archivo.
-        $name = 'blog_'.time().'.'.$file->getClientOriginalExtension();
-        // Ruta de la Imagen.
-        $path = public_path().'/img/articles/';
-        $file->move($path, $name);
-        //dd($path);
+
+        // Valido si Capturo el Archivo.
+        if ($request->file('image')) {
+            // Manipulacion de Imagen.
+            $file = $request->file('image');
+            // Nombre del archivo.
+            $name = 'blog_'.time().'.'.$file->getClientOriginalExtension();
+            // Ruta de la Imagen.
+            $path = public_path().'/img/articles/';
+            $file->move($path, $name);
+        }
+
+        // Instancio la variable 'article'.
+        $article = new Article($request->all());
+        // Capturo el id del usuario logueado.
+        $article->user_id = \Auth::user()->id;
+        // Guardo el articulo.
+        $article->save();
+
+
+        // Luego de guardar el articulo rellenamos la Tabla Pivot ...
+        // llamando la Fn 'tags()' del modelo 'Article' y usando la Fn 'sync()' ...
+        // Para rellenar la Tabla Pivot
+        $article->tags()->sync($request->tags);
+
+        // Instancio la variable 'image'.
+        $image = new Image();
+        // capturo el name de la imagen.
+        $image->name = $name;
+        // llamo a la Fn 'article()' del modelo 'Image' y asocio o relaciono ...
+        // la imagen con el articulo, usando la Fn 'associate()'
+        $image->article()->associate($article);
+        // guardo la imagen.
+        $image->save();
+        // Mensaje a Mostrar.
+        Flash::success('Se ha Creado el Articulo <b>'.$article->title.'</b> de Forma Satisfactoria!');
+        // Redireccionamiento a la Vista 'admin.articles.index'
+        return redirect()->route('admin.articles.index');
+
     }
 
     /**
