a05d5715e46407f6bfd49067e9dc9b9b7afaed09
oarodriguez <omargumer22@gmail.com>
2019-01-07 21:34:46 -0430

    uso de plugins para mejorar la vista del formulario de articulos...

--- resources/views/admin/articles/create.blade.php
+++ resources/views/admin/articles/create.blade.php
@@ -20,17 +20,17 @@
 
 					<div class="form-group">
 						{!! Form::label('category_id', 'Categoria') !!}
-						{!! Form::select('category_id', $categories, null, ['class'=>'form-control', 'placeholder'=>'Seleccione...', 'required']) !!}
+						{!! Form::select('category_id', $categories, null, ['class'=>'form-control select-cat', 'placeholder'=>'Seleccione...', 'required']) !!}
 					</div>
 
 					<div class="form-group">
 						{!! Form::label('content', 'Contenido') !!}
-						{!! Form::textarea('content', null, ['class'=>'form-control', 'required', 'rows'=>'4']) !!}
+						{!! Form::textarea('content', null, ['class'=>'form-control cont', 'required', 'rows'=>'4']) !!}
 					</div>
 
 					<div class="form-group">
 						{!! Form::label('tags', 'Tags') !!}
-						{!! Form::select('tags', $tags, null, ['class'=>'form-control', 'multiple', 'required']) !!}
+						{!! Form::select('tags[]', $tags, null, ['class'=>'form-control select-tag', 'multiple', 'required']) !!}
 					</div>
 
 					<div class="from-group">
@@ -46,4 +46,21 @@
 				{!! Form::close() !!}
 			</div>		
     </div>
+@endsection
+
+@section('js')
+	<script>
+		$('.select-tag').chosen({
+			placeholder_text_multiple: 'Seleccione un maximo de 3 tags',
+			max_selected_options: 3,
+			search_contains: true,
+			no_results_text: 'No Hay Resultados...'
+		});
+
+		$('.select-cat').chosen({
+			no_results_text: 'No Hay Resultados...'
+		});
+
+		$('.cont').trumbowyg();
+	</script>
 @endsection
