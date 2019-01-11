0cb5e4585e32f5c9682d945adf0704a0d5efd434
oarodriguez <omargumer22@gmail.com>
2019-01-10 20:52:38 -0430

    creacion del controlador para las imagenes con metodo index.

---
+++ app/Http/Controllers/ImagesController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Image;
+
+class ImagesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // Obtengo los datos de las imagenes.
+        $images = Image::all();
+        /* NOTA:
+         * Recorrido de las imagenes con la Fn 'each()' para Obtener 
+         * la Relacion entre Imagenes / Articulos.
+        */
+        $images->each(function($images) {
+            $images->article; // (ver Fn's del modelo 'Image').
+        });
+        // Retorno la vista 'index' con los datos.
+        return view('admin.images.index')
+            ->with('images',$images);
+    }
+
+}
