e6946f51f5ed0bd656503b6e0c2957fd7ef3ec45
oarodriguez <omargumer22@gmail.com>
2019-01-10 20:55:36 -0430

    vista principal de las imagenes.

---
+++ resources/views/admin/images/index.blade.php
@@ -0,0 +1,22 @@
+@extends('admin.template.main')
+
+@section('title', 'Listado de Imagenes')
+
+@section('content')
+<div class="container">
+	<div class="jumbotron">
+		<div class="container">
+			@foreach ($images as $img)
+				<div class="col-md-3">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<img src="/img/articles/{{ $img->name }}" class="img-responsive">
+						</div>
+						<div class="panel-footer">{{ $img->article->title }}</div>
+					</div>
+				</div>				
+			@endforeach
+		</div>
+	</div>
+</div>
+@endsection
