b40f08bf408ef067707c5fa9965d68575297ce56
oarodriguez <omargumer22@gmail.com>
2019-01-12 20:53:20 -0430

    se agregaron rutas en el navbar para la pag. principal del sistema.

--- resources/views/admin/template/partials/nav.blade.php
+++ resources/views/admin/template/partials/nav.blade.php
@@ -16,7 +16,7 @@
         <div id="navbar" class="collapse navbar-collapse">
           @if (Auth::user())
             <ul class="nav navbar-nav">
-              <li class="active"><a href="/">Inicio</a></li>
+              <li class="active"><a href="{{ route('admin.index') }}">Inicio</a></li>
               <li><a href="{{ route('admin.users.index') }}">Usuarios</a></li>
               <li><a href="{{ route('admin.categories.index') }}">Categorias</a></li>
               <li><a href="{{ route('admin.articles.index') }}">Articulos</a></li>
@@ -26,6 +26,7 @@
             </ul>
 
             <ul class="nav navbar-nav navbar-right">
+              <li><a href="{{ route('front.index') }}" target="_blank">Pagina Principal</a></li>
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                 <ul class="dropdown-menu">
