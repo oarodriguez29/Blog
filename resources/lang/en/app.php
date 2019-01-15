82c04fb294eea8faf21cf5508a587eb756e11909
oarodriguez <omargumer22@gmail.com>
2019-01-15 16:49:31 -0430

    archivos creados para la traduccion de mensajes personalizados, español/ingles -  ingles/español.

---
+++ resources/lang/en/app.php
@@ -0,0 +1,11 @@
+<?php 
+
+/* NOTA: Archivo Creado para Traduccion de 
+ * Mensajes Personalizado. 
+ */
+return [
+
+	'title_last_articles'	=>	'Last Articles',
+	'title_categories'		=>	'Categories',
+
+];
