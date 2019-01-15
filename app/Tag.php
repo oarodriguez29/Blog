82dadec65b84924f9b6cfb3266c14120d84e8a4b
oarodriguez <omargumer22@gmail.com>
2019-01-14 20:58:01 -0430

    se creo el Scope para la busqueda de los Tags...

--- app/Tag.php
+++ app/Tag.php
@@ -20,4 +20,17 @@ class Tag extends Model
 	{
 		return $query->where('name', 'LIKE', "%$name%");
 	}
+
+	/* Scope para ser usado en la busqueda de los Tags x name,
+	 * Relacionado al metodo 'searchTag' del controlador 'FrontController'
+	 */
+
+	public function scopeSearchTag($query, $name)
+	{
+		/* NOTA: El Metodo '$query' Recibe 3 parametros, 
+		 * ('el registro a buscar', 'la condicion', y el objeto o parametro)
+		 */
+		// retorno la busqueda.
+		return $query->where('name', '=', $name);
+	}	
 }
